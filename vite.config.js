487f927969e60395bc4ad78f0e97156198425986
smpdm-hub <database.smpdm@gmail.com>
2026-07-20 10:40:17 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs.plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: './', // 💡 PENTING: Menggunakan jalur relatif agar aset CSS/JS dibaca sempurna oleh GitHub Pages
 })
