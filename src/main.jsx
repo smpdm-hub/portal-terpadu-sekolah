702b9a48093904041e7c3704d37ef50186ceeb1e
smpdm-hub <database.smpdm@gmail.com>
2026-07-20 11:09:41 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// 💡 Import AppProvider agar seluruh komponen aplikasi terjangkau oleh Context
+import { AppProvider } from './context/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Bungkus komponen App di dalam AppProvider */}
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 )
