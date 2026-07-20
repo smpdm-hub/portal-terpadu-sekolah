441f6363ea93c5da799faf964e54f8f6afa21635
smpdm-hub <database.smpdm@gmail.com>
2026-07-20 10:59:27 +0700

    Fix comment formatting in vite.config.js
    Updated comment for clarity.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs.plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 💡 PENTING: Menggunakan jalur relatif agar aset CSS/JS dibaca sempurna oleh GitHub Pages
+  base: './', // Jalur relatif agar aset dibaca sempurna oleh GitHub Pages
 })
